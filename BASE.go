7ff4a292c1f924079d0cdeb40d9173ea6a65f141
Watcharapong Tassoongnern <brutal99@gmail.com>
2021-05-06 13:12:56 +0700

    Udate base

--- BASE.go
+++ BASE.go
@@ -1,16 +1,13 @@
 package minifiers
 
 import (
-	"fmt"
 	"strings"
 )
 
 func BASE(source string) string {
-	fmt.Println(source)
 	result := strings.TrimSpace(source)
 	result = strings.ReplaceAll(result, "\n", "")
 	result = strings.ReplaceAll(result, "\r", "")
 	result = strings.ReplaceAll(result, "\t", "")
-	fmt.Println(result)
 	return result
 }
--- CSS.go
+++ CSS.go
@@ -1,9 +1,6 @@
 package minifiers
 
-import "fmt"
-
 func CSS(source string) (string, error) {
-	fmt.Println("CSS")
 	result := BASE(source)
 	return result, nil
 }
