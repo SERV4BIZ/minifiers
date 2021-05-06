b64f9e9815d74e359277a3b5f5accec35def4e7c
Watcharapong Tassoongnern <brutal99@gmail.com>
2021-05-06 13:07:10 +0700

    Update BASE.go

--- BASE.go
+++ BASE.go
@@ -1,10 +1,15 @@
 package minifiers
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func BASE(source string) string {
+	fmt.Println(source)
 	result := strings.TrimSpace(source)
 	result = strings.ReplaceAll(result, "\n", "")
 	result = strings.ReplaceAll(result, "\r", "")
+	fmt.Println(result)
 	return result
 }
