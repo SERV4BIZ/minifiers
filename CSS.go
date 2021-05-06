5641de0105c3ba188358f5e423be2e231485e9d6
Watcharapong Tassoongnern <brutal99@gmail.com>
2021-05-06 12:49:43 +0700

    Update CSS.go

--- CSS.go
+++ CSS.go
@@ -1,5 +1,10 @@
 package minifiers
 
+import "strings"
+
 func CSS(source string) (string, error) {
-	return "", nil
+	result := strings.TrimSpace(source)
+	result = strings.ReplaceAll(result, "\n", "")
+	result = strings.ReplaceAll(result, "\r", "")
+	return result, nil
 }
