0a8892781021dcc2495fcd70133f319a996e0073
Watcharapong Tassoongnern <brutal99@gmail.com>
2021-05-06 12:52:19 +0700

    Update JS.go

--- JS.go
+++ JS.go
@@ -1,5 +1,10 @@
 package minifiers
 
+import "strings"
+
 func JS(source string) (string, error) {
-	return "", nil
+	result := strings.TrimSpace(source)
+	result = strings.ReplaceAll(result, "\n", "")
+	result = strings.ReplaceAll(result, "\r", "")
+	return result, nil
 }
