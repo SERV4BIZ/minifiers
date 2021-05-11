15bb468000592cc34c044a7f47929066f99d7aff
Watcharapong Tassoongnern <brutal99@gmail.com>
2021-05-12 01:10:51 +0700

    Update HTML.go

--- HTML.go
+++ HTML.go
@@ -1,17 +1,7 @@
 package minifiers
 
 func HTML(source string) (string, error) {
-	compJS, err := JS(source)
-	if err != nil {
-		return source, err
-	}
-
-	compCSS, err := CSS(compJS)
-	if err != nil {
-		return source, err
-	}
-
-	compXML, err := XML(compCSS)
+	compXML, err := XML(source)
 	if err != nil {
 		return source, err
 	}
